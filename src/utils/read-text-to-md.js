0b83c28ae0d52af5510406290440c3598ec0c974
Diego Pulido <diegopulido384@gmail.com>
2022-06-25 17:40:51 -0500

    All the basic commands are implemented

--- src/utils/read-text-to-md.js
+++ src/utils/read-text-to-md.js
@@ -1,16 +1,111 @@
 export default function markDownToHtml(text) {
 	const htmlLines = [];
 
-	text.split('\n').forEach(line => {
+	text.split('\n').forEach((line, lineIndex) => {
 		let formatedText = line;
 
+		formatedText = formatHr(formatedText);
+		formatedText = formatLists(formatedText);
 		formatedText = formatTextDecorations(formatedText);
 		formatedText = formatBlockquote(formatedText);
+		formatedText = formatLinks(formatedText);
 		formatedText = formatTitles(formatedText);
+		formatedText = formatCodeBlock(formatedText);
+		formatedText = formatLinesTitle(
+			formatedText,
+			lineIndex < text.split('\n').length
+				? text.split('\n')[lineIndex + 1]
+				: null
+		);
+
+		if (formatedText === line) {
+			formatedText = formatedText.concat('</br>');
+		}
 
 		htmlLines.push(formatedText);
 	});
 	return htmlLines.join('');
+}
+
+function formatLinks(formatedText) {
+	const expression =
+		(formatedText.match(/!\[.+\]\(\S+\)/g)
+			? formatedText.match(/!\[.+\]\(\S+\)/g)[0]
+			: null) ||
+		(formatedText.match(/\[.+\]\(\S+\)/g)
+			? formatedText.match(/\[.+\]\(\S+\)/g)[0]
+			: null) ||
+		null;
+	if (expression) {
+		const exp_text = expression.slice(
+			expression.indexOf('[') + 1,
+			expression.indexOf(']')
+		);
+		const exp_link = expression.slice(
+			expression.indexOf('(') + 1,
+			expression.indexOf(')')
+		);
+
+		const image = `<img src="${exp_link}" alt="${exp_text}" title="${exp_text}"/>`;
+		const link = `<a href="${exp_link}">${exp_text}</a>`;
+
+		formatedText = expression[0] == '!' ? image : link;
+	}
+	return formatedText;
+}
+
+function formatHr(formatedText) {
+	if (formatedText === '___' || formatedText === '***') {
+		console.log(formatedText);
+		formatedText = '<hr>';
+	}
+	return formatedText;
+}
+
+function formatLists(formatedText) {
+	if (formatedText.slice(0, 2).match(/- |\* |\+ /)) {
+		formatedText = `<li style="margin-left: ${
+			1 * (formatedText.match(/- |\* |\+ /g) || []).length * 10
+		}px;">${formatedText.replace(/- |\* |\+ /g, '')}</li>`;
+	}
+	if (formatedText.slice(0, 5).match(/-\[[xX]\] |-\[\] /)) {
+		formatedText = `<input type='checkbox' class='checkbox' onclick=(false) ${
+			formatedText[2] !== ']' ? 'checked' : ''
+		}> ${formatedText.slice(4)}`;
+	}
+	return formatedText.concat('</br>');
+}
+
+function formatLinesTitle(formatedText, textAfter) {
+	const titles = {
+		'=': '# ',
+		'-': '## ',
+	};
+	const text = [...new Set(textAfter)].join('');
+	const flag = [...new Set(formatedText)].join('');
+	if (textAfter && titles[text]) {
+		formatedText = formatTitles(titles[text] + formatedText);
+	} else if (titles[flag]) {
+		formatedText = '';
+	}
+	return formatedText;
+}
+
+let isOpenCodeBlock = false;
+function formatCodeBlock(formatedText) {
+	if (formatedText === '```') {
+		if (!isOpenCodeBlock) {
+			formatedText = "<code class='CodeBlock'><xmp>";
+			isOpenCodeBlock = true;
+		} else {
+			formatedText = '</xmp></code>';
+			isOpenCodeBlock = false;
+		}
+	}
+	if (isOpenCodeBlock) {
+		formatedText = formatedText.concat('\n');
+	}
+	return formatedText;
 }
 
 function formatBlockquote(formatedText) {
@@ -21,7 +116,7 @@ function formatBlockquote(formatedText) {
 			' </div>'.repeat((formatedText.match(/>/g) || []).length)
 		);
 	} else {
-		return formatedText.concat('</br>');
+		return formatedText;
 	}
 }
 
@@ -37,9 +132,12 @@ function formatTitles(formatedText) {
 				formatedText[titleTagIndex + 1] === '#')
 		) {
 			const value = formatedText.slice(titleTagIndex).split(' ');
-			formatedText = `${formatedText.slice(0, titleTagIndex)} <h${
-				value[0].length
-			}> ${value.slice(1).join(' ')} </h${value[0].length}>`;
+			formatedText = `${formatedText.slice(
+				0,
+				titleTagIndex
+			)} <h${Math.min(value[0].length, 6)}> ${value
+				.slice(1)
+				.join(' ')} </h${Math.min(value[0].length)}>`;
 		}
 	}
 	return formatedText;
@@ -67,45 +165,8 @@ function formatTextDecorations(formatedText) {
 
 /* 
 
-TODO: Dont show blocks of code with the ` character
+
 TODO: Improve the transition when the editor is resized
 TODO: Save the text in the editor when the user clicks on the save button
-
-
-===============	= h1
---------------- = h2
-
-
-- = <ul>
-+ = <ul>
-* = <ul>
-[] = <input type="checkbox">
-[x] = <input type="checkbox" checked>
-1. = <ol>
-\t - = <ul><ul>
-\t 1. = <ol><ol>
-`` = <code>
---- = <hr>
-*** = <hr>
-___ = <hr>
-
-[link](url) = <a href="url">link</a>
-<url> = <a src="url">
-![alt text](url) = <img alt="alt text" src="url">
-
-|a| b |
-|--|--|
-| b |b  | = <table>
-
-\ = escaping
-
-
-https://www.markdownguide.org/cheat-sheet/ = markdown cheat sheet
-
-
-
-
-
-
-
+TODO: Improve the blockquotes
 */
